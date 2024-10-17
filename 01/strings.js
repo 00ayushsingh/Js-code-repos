5f55a38ef62dce8dd604133c5aac34aeb38a0e0d
Ayush singh <97881634+00ayushsingh@users.noreply.github.com>
2024-10-17 12:49:06 +0000

    commity

---
+++ 01/strings.js
@@ -0,0 +1,8 @@
+//const name = "hello"
+//const repcount = 50
+//console.log(name + repcount + " value"); //hello50 value outdated syntax
+
+//console.log(`hello my name is ${name} and my repo count is ${repcount}`); // modern way known as string interpolation
+
+const str = "hellohowareyour";
+console.log(str.slice(-1, 1)); 
